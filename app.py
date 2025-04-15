6e646d1bd950a3604010967c62d4d27efc269d91
KimothoIbrahim <kimothoibrahim@yahoo.com>
2025-04-15 21:21:13 +0300

    Added the age function

--- app.py
+++ app.py
@@ -2,7 +2,13 @@
 """
 
 
-def talk(name):
+def talk(name: str):
     """regurge name
     """
     return f'My name is {name}'
+
+
+def age(age: int):
+    """Reveal my Age
+    """
+    return f'My age is {age}'
