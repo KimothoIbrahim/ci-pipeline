35c7510873905d041b018f7d66819676650e5e83
KimothoIbrahim <kimothoibrahim@yahoo.com>
2025-04-16 15:08:44 +0300

    Add a test for shout method

--- test_app.py
+++ test_app.py
@@ -9,3 +9,9 @@ class TestTalk:
         """unit test for talk function
         """
         assert app.talk('Ibrahim') == 'My name is Ibrahim'
+
+
+    def test_shout(self):
+        """Shout your name
+        """
+        assert app.shout('Ibrahim') == 'I am screaming out my name Ibrahim'
